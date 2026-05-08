7ddcd91f0140d6fc40155ac5a5dac2e99466807a
Damien <756225@student.dusd.net>
2026-05-08 10:43:30 -0700

    sdsd
    sdsd

--- PrietoP4Unit5/Assets/Course Library/Scripts/Target.cs
+++ PrietoP4Unit5/Assets/Course Library/Scripts/Target.cs
@@ -23,25 +23,30 @@ public class Target : MonoBehaviour
     {
         
     }
+    
+    private void OnMouseDown()
+    {
+        Destroy(gameObject);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Destroy(gameObject);
+    }
 
     Vector3 RandomForce()
     {
         return Vector3.up * Random.Range(minSpeed, maxSpeed);
     }
+
     float RandomTorque()
     {
         return Random.Range(-maxTorque, maxTorque);
     }
+
     Vector3 RandomSpawnPos()
     {
         return new Vector3(Random.Range(-xRange, xRange), yRange);
     }
-    private void OnMouseDown()
-    {
-        Destroy(gameObject);
-    }
-    private void OnTriggerEnter(Collider other)
-    {
-        Destroy(gameObject);
-    }
+    
 }
